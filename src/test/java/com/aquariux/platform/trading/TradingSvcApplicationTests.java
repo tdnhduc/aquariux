708830fda2fee8ccc6edacb55bd0f41480e04f5c
Duc Nguyen <ducnguyenpy1221@gmail.com>
2024-07-07 16:15:07 +0700

    initial commit

---
+++ src/test/java/com/aquariux/platform/trading/TradingSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquariux.platform.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
